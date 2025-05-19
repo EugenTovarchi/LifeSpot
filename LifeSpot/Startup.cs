338cba2bdb4ae9443e12b9a0c5e0ce9767513ace
Eugen T <rovanphone@gmail.com>
2025-05-19 12:23:38 +0300

    doesn't work placeholders
    # Conflicts:
    #	LifeSpot/Program.cs
    #	LifeSpot/Startup.cs
    #	LifeSpot/Static/CSS/index.css
    #	LifeSpot/Static/JS/about.js
    #	LifeSpot/Static/JS/index.js
    #	LifeSpot/Views/about.html
    #	LifeSpot/Views/index.html

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -10,75 +10,25 @@ namespace LifeSpot
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+       public void ConfigureServices(IServiceCollection services)
+       {
+       }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
+       public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+       {
+           if (env.IsDevelopment())
+               app.UseDeveloperExceptionPage();
 
-            app.UseRouting();
+           app.UseRouting();
 
-            // Загружаем отдельные элементы для вставки в шаблон: боковое меню и футер
-            string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
-            string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sideBar.html"));
+           app.UseEndpoints(endpoints =>
+           {
+               // РњР°РїРїРёРЅРі СЃС‚Р°С‚РёС‡РµСЃРєРёС… С„Р°Р№Р»РѕРІ
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-
-                    // Загружаем шаблон страницы, вставляя в него элементы
-                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                        .Replace("<!--SIDEBAR-->", sideBarHtml)
-                        .Replace("<!--FOOTER-->", footerHtml);
-
-                    await context.Response.WriteAsync(html.ToString());
-                });
-
-                endpoints.MapGet("/testing", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
-
-                    // Загружаем шаблон страницы, вставляя в него элементы
-                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
-                        .Replace("<!--SIDEBAR-->", sideBarHtml)
-                        .Replace("<!--FOOTER-->", footerHtml);
-
-                    await context.Response.WriteAsync(html.ToString());
-                });
-
-                endpoints.MapGet("/Static/CSS/index.css", async context =>
-                {
-                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
-                    var css = await File.ReadAllTextAsync(cssPath);
-                    await context.Response.WriteAsync(css);
-                });
-
-                endpoints.MapGet("/Static/JS/index.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
-
-                endpoints.MapGet("/Static/JS/about.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
-
-                endpoints.MapGet("/Static/JS/input.js", async context =>
-                {
-                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "input.js");
-                    var js = await File.ReadAllTextAsync(jsPath);
-                    await context.Response.WriteAsync(js);
-                });
-            });
-        }
+               endpoints.MapCss();
+               endpoints.MapJs();
+               endpoints.MapHtml();
+           });
+       }
     }
 }
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,19 +1,81 @@
-﻿function filterContent() { 
-
-    let inputValue = inputParseFunction();
-    // Находим контейнеры с видео, которые необходимо фильтровать
-    let elements = document.getElementsByClassName('video-container');
-
-    // Пробегаемся по контейнерам
-    for (let i = 0; i <= elements.length; i++) {
-        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-        let videoText = elements[i].querySelector(".video-title").innerText;
-
-        if (!videoText.toLowerCase().includes(inputValue.toLowerCase())) {
-            elements[i].style.display = 'none'; // Скрыть, если не подходит
+let checker = function (newVisit)
+{
+    if(window.sessionStorage.getItem("userAge")>= 18)
+    {
+        if(newVisit)
+        {
+            alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString());
         }
-        else {
-            elements[i].style.display = 'inline-block'; // Показать, если подходит           
+        else
+        {
+            alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
+            window.location.href = "http://www.google.com"
         }
     }
 }
+
+function handlingSession ()
+{
+    if (!sessionStorage.getItem("startDate"))
+    {
+        sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+    if(!sessionStorage.getItem("userAgent"))
+    {
+        sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+    if(!sessionStorage.getItem("userAge"))
+    {
+        sessionStorage.setItem("userAge", prompt("Введите ваш возраст:"))
+        initEnter();
+    }
+}
+
+function initEnter ()
+{
+    let newVisit = {
+        userAgent:sessionStorage.getItem("userAgent"),
+        startDate:sessionStorage.getItem("starDate"),
+        userAge:sessionStorage.getItem("userAge")
+    }
+    checker(newVisit);
+}
+
+/*
+* Вывод данных сессии в консоль
+ */
+let sessionLog = function (){
+    console.log("Начало сессии: " + sessionStorage.getItem("startDate"))
+    console.log("Ваш пользователя:"+ sessionStorage.getItem("userAge"))
+    console.log("Данные клиента:" + sessionStorage.getItem("userAgent"))
+}
+/*
+* Функция для фильтрации контента
+* Будет вызываться благодаря атрибуту oninput на index.html.
+* */
+
+function filterContent(){
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+/*
+* Всплывающее окно будет показано по таймауту
+* 
+* */
+// setTimeout(() =>
+//     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+// 7000);
+
+
+
+
