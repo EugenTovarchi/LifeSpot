f599ef93f9b50d9aa86a1c34894d8393a1e5beb0
Eugen T <rovanphone@gmail.com>
2025-05-20 10:17:30 +0300

    add using to Startup.cs

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
 public class Startup
 {
    public void ConfigureServices(IServiceCollection services)
